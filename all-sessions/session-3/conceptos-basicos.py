ad32795d520d028ac1b26db3efb03957171c19c1
Tadeo Marino <100310429+tadeomarino@users.noreply.github.com>
2024-03-15 08:28:43 -0300

    Uploading session 9

---
+++ all-sessions/session-3/conceptos-basicos.py
@@ -0,0 +1,28 @@
+# Conceptos básicos
+
+'''
+    En esta segunda sesión vamos a conocer varios conceptos fundamentales del lenguaje.
+    
+    Python es un lenguaje de programación de alto nivel, interpretado y de propósito general. 
+    Su diseño se centra en la legibilidad del código, lo que permite que los programas escritos 
+    en Python sean fáciles de leer y de mantener.
+
+    1. Variables
+    2. 
+
+    1. Comenzaremos con las variables, que son los bloques de construcción básicos de cualquier programa. 
+    Una variable es simplemente un nombre que se refiere a un valor. En Python, las variables son dinámicamente tipadas,
+    lo que significa que puedes reasignar variables a diferentes tipos de datos.
+
+    2. A continuación, aprenderemos sobre la concatenación de variables, que es la unión de dos o más cadenas de texto.
+    Esto es útil cuando quieres combinar texto y variables para crear mensajes personalizados.
+
+    3. Luego, nos adentraremos en las listas, que son una colección de elementos. Las listas son uno de los tipos de datos
+    más utilizados en Python, y son increíblemente versátiles.
+
+    4. Después, exploraremos los condicionales, que nos permiten tomar decisiones en nuestro código. Los condicionales son
+    una parte esencial de cualquier lenguaje de programación y son fundamentales para la creación de programas interactivos.
+
+    Al dominar estos conceptos, estaremos bien equipados para crear programas de automatización eficientes y efectivos con Python.
+    En futuras sesiones, aprenderemos más sobre cómo podemos aplicar estos conceptos para automatizar tareas repetitivas y ahorrar tiempo.
+'''
